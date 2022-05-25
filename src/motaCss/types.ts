4ffe7cdcd9a3279c42bf8276b7ec04fada8064f6
wiloke1 <hpvanlong@gmail.com>
2022-05-25 09:20:11 +0700

    add comment for IMotaCss

--- src/motaCss/types.ts
+++ src/motaCss/types.ts
@@ -1,11 +1,88 @@
 export interface IMotaCss {
-  find(html: string): void;
+  /**
+   * Find the class names in the string
+   * @example
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * atomic
+   *   .find(`<div class="c:red c:blue|h fz:20px w:30%@md p:30px@md m:20px@+300px pos:relative!"></div>`);
+   *   .find(`const className = "bgc:blue";`);
+   * ```
+   */
+  find(value: string): void;
+  /**
+   * Set the config
+   */
   setConfig(config: Config): void;
+  /**
+   * Get the css
+   * @example
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * const cssContent = atomic.getCss();
+   * ```
+   */
   getCss(): string;
+  /**
+   * Custom css values
+   * @example
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * atomic.customValue(value => {
+   *   console.log(value);
+   *   return value;
+   * });
+   * ```
+   */
   customValue(callback: CustomValue): void;
+  /**
+   * Set the class names
+   * @example
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * atomic.setClassNames(['c:red', 'bgc:blue']);
+   * ```
+   */
   setClassNames(classNames: string[]): void;
+  /**
+   * Add syntax for properties
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * atomic.addPropsSyntax({
+   *   co: 'color',
+   *   test: 'background-color',
+   * });
+   * ```
+   */
   addPropsSyntax(props: CssProps): void;
+  /**
+   * Add syntax for pseudo
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * atomic.addPseudoSyntax({
+   *   ho: 'hover',
+   *   abc: 'focus',
+   * });
+   * ```
+   */
   addPseudoSyntax(pseudo: Pseudo): void;
+  /**
+   * Subscribe to the css
+   * ```ts
+   * import { atomic } from 'mota-css';
+   *
+   * const unsubscribe = atomic.subscribe((css) => {
+   *   console.log(css);
+   *   // or console.log(atomic.getCss());
+   * });
+   * ```
+   */
   subscribe(listener: Listener): Unsubscribe;
 }
 
